770921ad733718dbb254bea738f87aeaf4f26684
Shamaoke <shamaoke@yandex.ru>
2018-01-21 17:39:16 +0300

    Initialize the polybar project

---
+++ polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# Terminate already running bar instances
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+
+while pgrep -x polybar > /dev/null; do sleep 1; done
+
+# Launch example
+
+polybar --reload sample &
+
+echo "Bars launched..."
