a4798d37a9eb706a7ecdbf8a45c8b1e68e0f05ed
Shamaoke <shamaoke@yandex.ru>
2019-04-02 08:29:46 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -1,4 +1,4 @@
 // an example to create a new mapping `ctrl-y`
 mapkey('<Ctrl-y>', 'Show me the money', function() {
-  Front.showPopup('Hello, I\'m a rabbit. Catch me if you can!')
+  Front.showPopup(settings)
 })
