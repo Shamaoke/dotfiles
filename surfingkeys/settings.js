708de47519150d61f80ec53e590f2169be706441
Shamaoke <shamaoke@yandex.ru>
2019-04-02 18:12:06 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -1,27 +1,27 @@
 settings.hintAlign = 'left'
 
-Hints.style(`
+// Hints.style(`
+//
+//   background: unset !important;
+//   border:     unset !important;
+//   font:       unset !important;
+//
+//   background-color: #ff6c00           !important;
+//   font-family:      Input Mono Narrow !important;
+//   font-weight:      normal            !important;
+//   border-width:     2px               !important;
+//
+// `)
 
-  background: unset !important;
-  border:     unset !important;
-  font:       unset !important;
-
-  background-color: #ff6c00           !important;
-  font-family:      Input Mono Narrow !important;
-  font-weight:      normal            !important;
-  border-width:     2px               !important;
-
-`)
-
-Hints.style(`
-
-  background: unset !important;
-  border:     unset !important;
-  font:       unset !important;
-
-  background-color: #008844           !important;
-  font-family:      Input Mono Narrow !important;
-  font-weight:      normal            !important;
-  border-width:     2px               !important;
-
-`, 'text')
+// Hints.style(`
+//
+//   background: unset !important;
+//   border:     unset !important;
+//   font:       unset !important;
+//
+//   background-color: #008844           !important;
+//   font-family:      Input Mono Narrow !important;
+//   font-weight:      normal            !important;
+//   border-width:     2px               !important;
+//
+// `, 'text')
