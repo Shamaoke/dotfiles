c7d13ab4febfaac62b7e6945ede9ea78d61aca20
Shamaoke <shamaoke@yandex.ru>
2019-04-02 10:23:42 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -1,5 +1,5 @@
-settings.hintAlign = 'left'
+settings.hintAlign = 'left';
 
-Hints.style('background-color: #006cff')
+Hints.style('border: solid 3px #552a48; color:#efe1eb; background: initial; background-color: #552a48;');
 
-Hints.style('background-color: #ff6c00', 'text')
+Hints.style("border: solid 8px #C38A22;padding: 1px;background: #e39913", "text");
