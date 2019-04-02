bb573e7acb5ae2321132e644592b6e61c4de4cc1
Shamaoke <shamaoke@yandex.ru>
2019-04-02 09:01:55 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -1,1 +1,18 @@
 settings.hintAlign = 'left'
+
+settings.theme = `
+
+#sk_status, #sk_find {
+  font-size: 0.5em;
+}
+
+#sk_hints > div {
+  font-size:     2em;
+  font-weight:   normal;
+  padding:       0px 4px 0px 4px;
+  color:         #ff6c00;
+  border:        solid 2px #C38A22;
+  border-radius: unset;
+}
+
+`
