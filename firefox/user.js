53e0bd6786f3bc1dda1658a2bd76f33725a732fc
Shamaoke <shamaoke@yandex.ru>
2019-04-02 08:18:43 +0300

    [firefox] edit user.js

--- firefox/user.js
+++ firefox/user.js
@@ -1,11 +1,11 @@
 // show 24 tiles at the home page instead of the default 4
-user_pref('browser.newtabpage.activity-stream.topSitesCount', 24);
+// user_pref('browser.newtabpage.activity-stream.topSitesCount', 24);
 
 // do not close the browser window when closing the last tab
 user_pref('browser.tabs.closeWindowWithLastTab', false);
 
 // do not show the search field in the preferences tab
-user_pref('browser.preferences.search', false);
+// user_pref('browser.preferences.search', false);
 
 // disable the pocket extension
 user_pref('extensions.pocket.enabled', false);
@@ -14,7 +14,7 @@ user_pref('extensions.pocket.enabled', false);
 user_pref('layers.acceleration.force-enabled', true);
 
 // disable the reader mode
-// user_pref('reader.parse-on-load.enabled', false);
+user_pref('reader.parse-on-load.enabled', false);
 
 // disable showing the menu by pressing Alt
 // user_pref('ui.key.menuAccessKey', 0);
@@ -27,25 +27,43 @@ user_pref('ui.key.menuAccessKey', 18);
 user_pref('ui.key.menuAccessKeyFocuses', false);
 
 // enable extensions on addons.mozilla.org
-user_pref('privacy.resistFingerprinting.block_mozAddonManager', true);
+// user_pref('privacy.resistFingerprinting.block_mozAddonManager', true);
 
 // set the addres bar drop-down list length
-user_pref('browser.urlbar.maxRichResults', 20);
+// user_pref('browser.urlbar.maxRichResults', 20);
 
 // disable video autoplay
-user_pref('media.autoplay.enabled', false);
+// user_pref('media.autoplay.enabled', false);
 
 // use a configuration file for proxy settings
 user_pref('network.proxy.type', 2);
 
 // set a configuration file for proxy server settings
-user_pref('network.proxy.autoconfig_url', 'file:///home/user/.mozilla/firefox/304it05e.default/proxy.pac');
+user_pref('network.proxy.autoconfig_url', 'file:///home/user/.mozilla/firefox/mr70dnal.default/proxy.pac');
 
 // show URL scheme in address bar
-user_pref('browser.urlbar.trimURLs', false);
+// user_pref('browser.urlbar.trimURLs', false);
 
 // suppress web notifications requests for all sites
 user_pref('dom.webnotifications.enabled', false);
+
+// restore previous session when starting the browser
+user_pref('browser.startup.page', 3);
+
+// set the homepage
+user_pref('browser.startup.homepage', 'about:blank');
+
+// set the new tab page as a blank page
+user_pref('browser.newtabpage.enabled', false);
+
+// remove one click search engines from url bar drop-down list
+user_pref('browser.urlbar.oneOffSearches', false);
+
+// don't install "studies" see: https://support.mozilla.org/en-US/kb/shield
+user_pref('app.shield.optoutstudies.enabled', false);
+
+// disable personalized extension recommendations
+// user_pref('browser.newtabpage.activity-stream.asrouter.userprefs.cfr', false);
 
 /* :::Fonts::: */
 
@@ -56,46 +74,46 @@ user_pref('font.default.x-western', 'sans-serif');
 user_pref('font.default.x-cyrillic', 'sans-serif');
 
 // set a font family for the Latin serif type
-user_pref('font.name.serif.x-western', 'PT Serif');
+// user_pref('font.name.serif.x-western', 'PT Serif');
 
 // set a font family for the Cyrillic serif type
-user_pref('font.name.serif.x-cyrillic', 'PT Serif');
+// user_pref('font.name.serif.x-cyrillic', 'PT Serif');
 
 // set a font family for the Japanese serif type
-user_pref('font.name.serif.ja', 'Kozuka Mincho Pro');
+// user_pref('font.name.serif.ja', 'Kozuka Mincho Pro');
 
 // set a font family for the Latin sans-serif type
-user_pref('font.name.sans-serif.x-western', 'PT Sans');
+// user_pref('font.name.sans-serif.x-western', 'PT Sans');
 
 // set a font family for the Cyrillic sans-serif type
-user_pref('font.name.sans-serif.x-cyrillic', 'PT Sans');
+// user_pref('font.name.sans-serif.x-cyrillic', 'PT Sans');
 
 // set a font family for the Japanese sans-serif type
-user_pref('font.name.sans-serif.ja', 'Kozuka Gothic Pro');
+// user_pref('font.name.sans-serif.ja', 'Kozuka Gothic Pro');
 
 // set a font family for the Latin monospace type
-user_pref('font.name.monospace.x-western', 'Input Mono Narrow');
+// user_pref('font.name.monospace.x-western', 'Input Mono Narrow');
 
 // set a font family for the Cyrillic monospace type
-user_pref('font.name.monospace.x-cyrillic', 'Input Mono Narrow');
+// user_pref('font.name.monospace.x-cyrillic', 'Input Mono Narrow');
 
 // set a font family for the Japanese monospace type
-user_pref('font.name.monospace.ja', 'Input Mono Narrow');
+// user_pref('font.name.monospace.ja', 'Input Mono Narrow');
 
 // set a size for the Latin proportional fonts
-user_pref('font.size.variable.x-western', 16);
+// user_pref('font.size.variable.x-western', 16);
 
 // set a size for the Cyrillic proportional fonts
-user_pref('font.size.variable.x-cyrillic', 16);
+// user_pref('font.size.variable.x-cyrillic', 16);
 
 // set a size for the Japanese proportional fonts
-user_pref('font.size.variable.ja', 16);
+// user_pref('font.size.variable.ja', 16);
 
 // set a size for the Latin monospace fonts
-user_pref('font.size.fixed.x-western', 14);
+// user_pref('font.size.fixed.x-western', 14);
 
 // set a size for the Cyrillic monospace fonts
-user_pref('font.size.fixed.x-cyrillic', 14);
+// user_pref('font.size.fixed.x-cyrillic', 14);
 
 // set a size for the Japanese monospace fonts
-user_pref('font.size.fixed.ja', 14);
+// user_pref('font.size.fixed.ja', 14);
