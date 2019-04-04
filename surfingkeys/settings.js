3838109271aae5b9d05cff6d79d296507917b4e6
Shamaoke <shamaoke@yandex.ru>
2019-04-04 19:16:52 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -6,14 +6,10 @@ settings.hintAlign = 'left'
 // :::bindings:::
 
 // open a link in a new tab
-mapkey('F', '#1Open a link in new tab', function() {
-  Hints.create('', Hints.dispatchMouseClick, {tabbed: true})
-})
+mapkey( 'F', '#1Open a link in a new tab', ( ) => Hints.create('', Hints.dispatchMouseClick, {tabbed: true}) )
 
 // open a link in a current tab
-mapkey('f', '#1Open a link in the current tab; press Shift to flip hints if they are overlapped', function() {
-  Hints.create('', Hints.dispatchMouseClick)
-})
+mapkey( 'f', '#1Open a link in the current tab; press Shift to flip hints if the are overlapped', ( ) => Hints.create('', Hints.dispatchMouseClick) )
 
 // unmap a key for opening a new window with the current tab
 unmap('W')
