eb5b50ca2d1c69c841f950a691afc49122d18b6d
Shamaoke <shamaoke@yandex.ru>
2019-04-08 15:37:14 +0300

    [surfingkeys] edit settings

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -8,8 +8,26 @@ settings.hintAlign = 'left'
 // open a link in a new tab
 mapkey( 'F', '#1Open a link in a new tab', ( ) => Hints.create('', Hints.dispatchMouseClick, {tabbed: true}) )
 
-// open a link in a current tab
-mapkey( 'f', '#1Open a link in the current tab;<br /> press Shift to flip hints if the are overlapped', ( ) => Hints.create('', Hints.dispatchMouseClick) )
+// open a link in the current tab
+mapkey( 'f', '#1Open a link in the current tab;<br /> press Shift to flip hints if they are overlapped', ( ) => Hints.create('', Hints.dispatchMouseClick) )
+
+// go back in history
+mapkey( '<', '#Go back in history', ( ) => history.go(-1), {repeatIgnore: true} )
+
+// go forward in history
+mapkey( '>' '#4Go forward in history', ( ) => history.go(1), {repeatIgnore: true} )
+
+// go one tab left
+mapkey( 'h', '#Go one tab left', ( ) => RUNTIME('previousTab'), {repeatIgnore: true})
+
+// go one tab right
+mapkey( 'l', '#Go one tab right', ( ) => RUNTIME('nextTab'), {repeatIgnore: true})
+
+// move the current tab to the left
+mapkey( 'H', '#3Move the current tab to the left', ( ) => RUNTIME('moveTab', {step: -1}) )
+
+// move the current tab to the right
+mapkey( 'L', '#3Move the current tab to the right', ( ) => RUNTIME('moveTab', {step: 1}) )
 
 // unmap a key for opening a new window with the current tab
 unmap('W')
