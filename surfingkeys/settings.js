58763d7346197ee66322705c4a546b1b4ca7f976
Shamaoke <shamaoke@yandex.ru>
2021-05-07 14:46:38 +0300

    [surfingkeys] edit settings.js

--- surfingkeys/settings.js
+++ surfingkeys/settings.js
@@ -64,29 +64,3 @@ Hints.style(`
   text-transform: lowercase         !important;
 `)
 
-// Hints.style(`
-//
-//   background: unset !important;
-//   border:     unset !important;
-//   font:       unset !important;
-//
-//   background-color: #ff6c00           !important;
-//   font-family:      Input Mono Narrow !important;
-//   font-weight:      normal            !important;
-//   border-width:     2px               !important;
-//
-// `)
-
-// Hints.style(`
-//
-//   background: unset !important;
-//   border:     unset !important;
-//   font:       unset !important;
-//
-//   background-color: #008844           !important;
-//   font-family:      Input Mono Narrow !important;
-//   font-weight:      normal            !important;
-//   border-width:     2px               !important;
-//
-// `, 'text')
-
