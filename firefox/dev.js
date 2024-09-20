01c47f66497b301e28ad3eb41522ed4a60432e71
Shamaoke <shamaoke@yandex.ru>
2024-09-20 15:17:42 +0300

    [firefox] create dev.js

---
+++ firefox/dev.js
@@ -0,0 +1,10 @@
+
+// Do not close the browser window when closing the last tab
+user_pref('browser.tabs.closeWindowWithLastTab', false);
+
+// Prevent save password popup from appearing when entering passwords in password fields
+user_pref('signon.rememberSignons', false);
+
+// Disable build-in translator
+user_pref('browser.translations.enable', false);
+
