794d79404c2943abd819342c09b858d8a1be2fb3
Shamaoke <shamaoke@yandex.ru>
2024-12-19 19:50:05 +0300

    [firefox] edit tor.js

---
+++ firefox/tor.js
@@ -0,0 +1,15 @@
+
+///
+/// ::: Firefox configuration for the TOR profile :::
+///
+
+/// When restoring a session, disable opening the browser on the virtual desktop
+/// where it was closed last time
+user_pref('widget.disable-workspace-management', true);
+
+/// Use a configuration file for proxy settings
+user_pref('network.proxy.type', 2);
+
+/// Set the configuration file for proxy server settings
+user_pref('network.proxy.autoconfig_url', 'file:///home/user/.mozilla/firefox/8z23nxxa.tor/proxy.pac');
+
