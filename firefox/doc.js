e2188d6ff7d54d73fb6dfbd3c2cfad50ae7b60be
Shamaoke <shamaoke@yandex.ru>
2024-12-19 21:44:59 +0300

    [firefox] edit doc.js

---
+++ firefox/doc.js
@@ -0,0 +1,22 @@
+
+///
+/// ::: Firefox configuration for the DOC profile :::
+///
+
+/// Do not close the browser window when closing the last tab
+user_pref('browser.tabs.closeWindowWithLastTab', false);
+
+/// When restoring a session, disable opening the browser on the virtual desktop
+/// where it was closed last time
+user_pref('widget.disable-workspace-management', true);
+
+/// Set the Alt key to show menu
+user_pref('ui.key.menuAccessKey', 18);
+
+/// Do not show menu by pressing the Alt key alone. Use the key accelerators
+/// along with the Alt key instead
+user_pref('ui.key.menuAccessKeyFocuses', false);
+
+/// Behave in the window mode as in the fullscreen mode
+user_pref('full-screen-api.ignore-widgets', true);
+
